03dbe4d88bdcef6b73966f986185e11d382fa725
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-06-25 01:42:32 +0300

    Initial Commit

---
+++ homework03.2.28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework03.2.28
+//
+//  Created by Daniil Lebedev on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
