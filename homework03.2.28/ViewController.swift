f3db541d8c86f0785e2532613c8e6d4d70a47fb7
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-06-26 14:50:14 +0300

    checkpoint

--- homework03.2.28/ViewController.swift
+++ homework03.2.28/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK: - IB Outlets
+    @IBOutlet var colorCheckView: UIView!
+    
+    @IBOutlet var redNameLabel: UILabel!
+    @IBOutlet var redNumberLabel: UILabel!
+    @IBOutlet var redColorSlider: UISlider!
+    
+    @IBOutlet var greenNameLabel: UILabel!
+    @IBOutlet var greenNumberLabel: UILabel!
+    @IBOutlet var greenColorSlider: UISlider!
+    
+    @IBOutlet var blueNameLabel: UILabel!
+    @IBOutlet var blueNumberLabel: UILabel!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redNameLabel.text = "red"
+        redNumberLabel.text = String(format: "%.2f", redColorSlider.value)
+        
+        greenNameLabel.text = "green"
+        greenNumberLabel.text = String(format: "%.2f", greenColorSlider.value)
+        
+        blueNameLabel.text = "blue"
+        blueNumberLabel.text = String(format: "%.2f", blueColorSlider.value)
+        
+        colorCheckView.layer.backgroundColor = CGColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        
+       
     }
-
-
+// MARK: - IB Actions
+    @IBAction func redCollorDrug() {
+    }
+    @IBAction func greenCollorDrug() {
+    }
+    @IBAction func blueCollorDrug() {
+    }
+    
+    
+    
+    
+    
+    // MARK: - Private metods
+    
 }
 
